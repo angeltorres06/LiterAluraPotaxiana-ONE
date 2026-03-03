05a9af73208fc386caf3da6e2af02f65e5bf5300
spiny006 <angeljworld@hotmail.com>
2026-03-02 22:05:00 -0600

    Inicio de  proyecto potaxiano. Salve Jiafei

---
+++ src/main/java/com/tonytorreslap/LiterAluraPotaxiana/LiterAluraPotaxianaApplication.java
@@ -0,0 +1,13 @@
+package com.tonytorreslap.LiterAluraPotaxiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraPotaxianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraPotaxianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonytorreslap/LiterAluraPotaxiana/LiterAluraPotaxianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonytorreslap.LiterAluraPotaxiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraPotaxianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
